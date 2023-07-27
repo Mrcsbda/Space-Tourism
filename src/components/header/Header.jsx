4565d1e2464f9c5a19330d012051497edba903e1
snaidy giraldo <snaidy98g@gmail.com>
2023-07-27 11:08:39 -0600

    create structure header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import Nav from '../nav/Nav'
-
+import React from "react";
+import Nav from "../nav/Nav";
+import "./header.scss";
+import logo from "../../../public/images/shared/logo.svg";
+import menu from "../../../public/images/shared/icon-hamburger.svg";
 const Header = () => {
   return (
-    <header>
-      <Nav/>
+    <header className="header">
+      {/* <Nav/> */}
+      <div className='header__up'>
+        <figure className='logo'>
+          <img src={logo} alt="" />
+        </figure>
+        <figure className='menu'>
+        <img src={menu} alt="" />
+        </figure>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-
+import './nav.scss'
 const Nav = () => {
     return (
         <nav>
