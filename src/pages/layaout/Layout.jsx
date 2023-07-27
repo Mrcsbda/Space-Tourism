afb8c3b67ed6710aa6b5997517255cf08e7e4dba
mrcsbda <macbedoya@gmail.com>
2023-07-27 10:08:01 -0500

    layout background

--- src/pages/layaout/Layout.jsx
+++ src/pages/layaout/Layout.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../../components/header/header'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import "./layout.scss"
 
+
 const Layout = () => {
+    const location = useLocation()
+    const [styleBackground, setStyleBackgrund] = useState("")
+
+    useEffect(() => {
+        changeBackground()
+    }, [location])
+
+    const changeBackground = () => {
+        switch (true) {
+            case location.pathname.includes("destination"):
+                setStyleBackgrund("destination")
+                break;
+            case location.pathname.includes("crew"):
+                setStyleBackgrund("crew")
+                break;
+            case location.pathname.includes("technology"):
+                setStyleBackgrund("technology")
+                break;
+            case location.pathname === "/":
+                setStyleBackgrund("home")
+                break
+            default: ""
+                break;
+        }
+    }
+
     return (
-        <>
+        <div className={styleBackground}>
             <Header />
-            <Outlet/>
-        </>
+            <Outlet />
+        </div>
     )
 }
 
