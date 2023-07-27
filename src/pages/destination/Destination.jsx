3f71c31d61b97215db03b7a98133216a7f1346ea
mrcsbda <macbedoya@gmail.com>
2023-07-27 11:59:26 -0500

    :lipstick: destination styles

--- src/pages/destination/Destination.jsx
+++ src/pages/destination/Destination.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { NavLink, useParams } from 'react-router-dom'
 import DestinationInfo from '../../components/destinationInfo/DestinationInfo'
 import { filterDestination } from '../../services/destination'
+import "./destination.scss"
 
 const Destination = () => {
   const { destinationName } = useParams()
@@ -15,25 +16,29 @@ const Destination = () => {
     <>
       {
         destination?.name && (
-          <section className='destination-container'>
+          <section className='destination'>
             <h2 className='destination__subtitle'><span>01</span> PICK YOUR DESTINATION</h2>
-            <img className='destination__image' src={destination.images.png} alt={destination.name} />
-            <div>
-              <nav>
-                <li>
-                  <NavLink to="/destination/Moon">Moon</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/destination/Mars">Mars</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/destination/Europa">Europa</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/destination/Titan">Titan</NavLink>
-                </li>
-              </nav>
-              <DestinationInfo destination={destination}/>
+            <div className='destination__content-container'>
+              <img className='destination__image' src={destination.images.png} alt={destination.name} />
+              <div className='destination__content'>
+                <nav className='destination__nav'>
+                  <ul className='destination__nav__list'>
+                    <li>
+                      <NavLink className='destination__nav__list--item' to="/destination/Moon">Moon</NavLink>
+                    </li>
+                    <li>
+                      <NavLink className='destination__nav__list--item' to="/destination/Mars">Mars</NavLink>
+                    </li>
+                    <li>
+                      <NavLink className='destination__nav__list--item' to="/destination/Europa">Europa</NavLink>
+                    </li>
+                    <li>
+                      <NavLink className='destination__nav__list--item' to="/destination/Titan">Titan</NavLink>
+                    </li>
+                  </ul>
+                </nav>
+                <DestinationInfo destination={destination} />
+              </div>
             </div>
           </section>
         )
