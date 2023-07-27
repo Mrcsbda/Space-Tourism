b58a1aaca2e1382292cc9f806f8363e629ed75d8
Stefannya <stefannyaonofre@gmail.com>
2023-07-27 11:06:48 -0500

    inicio maquetacion

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import './home.scss';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home">
+      <div className="home__container">
+        <span>SO, YOU WANT TO TRAVEL TO</span>
+        <h1>SPACE</h1>
+        <p>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
+      <figure className="home__figure">
+        <img src="images/group.png" />
+      </figure>
+      {/* <div className="home__btn">
+        <span>EXPLORE</span>
+      </div> */}
+    </section>
+  );
+};
 
-export default Home
+export default Home;
