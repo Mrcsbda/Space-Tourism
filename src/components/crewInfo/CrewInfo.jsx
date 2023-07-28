342200b5612934a0d04ca762d904b957b0429803
Stefannya <stefannyaonofre@gmail.com>
2023-07-28 11:30:07 -0500

    maquetacion crew

---
+++ src/components/crewInfo/CrewInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './crewInfo.scss';
+
+const CrewInfo = () => {
+  return (
+    <div className='crew-info'>
+        <h4 className='crew-info__subtitle'>COMMANDER</h4>
+        <h1 className='crew-info__title'>DOUGLAS HURLEY</h1>
+        <p className='crew-info__description'>Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
+    </div>
+  )
+}
+
+export default CrewInfo
--- src/pages/crew/Crew.jsx
+++ src/pages/crew/Crew.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { NavLink, useNavigate, useParams } from "react-router-dom";
+import { AppContext } from "../../routes/Router";
+import CrewInfo from "../../components/crewInfo/CrewInfo";
+import "./crew.scss";
 
 const Crew = () => {
-  return (
-    <div>Crew</div>
-  )
-}
+  // const navigate = useNavigate();
+  const crews = [
+    "Douglas Hurley",
+    "Mark Shuttleworth",
+    "Victor Glover",
+    "Anousheh Ansari",
+  ];
+  // const { setCrewPathName } = useContext(AppContext);
+  // const [crew, setCrew] = useState({});
+  // const { crewName } = useParams();
 
-export default Crew
+  // useEffect(() => {
+  //   setCrewPathName(crewName);
+  //   setCrew(crewName);
+  // }, [crewName]);
+
+  // const handleCrew = (crew) => {
+  //   setCrewPathName(crew);
+  //   navigate(`crew/${crew}`);
+  // };
+
+  return (
+    <>
+      {/* {crew?.name && (
+        <section className="crew">
+          <h2 className="crew__subtitle">
+            <span>02</span>Meet your crew
+          </h2>
+          <div className="crew__container">
+            <img
+              className="crew__image"
+              src="/images/crew/image-douglas-hurley.png"
+            />
+            <hr />
+            <div className="crew__info">
+              <nav className="crew__nav">
+                <ul className="crew__nav__lis">
+                  {crews.map((item, index) => (
+                    <li
+                      key={index + 1}
+                      className={`${crewName === item && "active-crew"}`}
+                      onClick={() => handleCrew(item)}
+                    ></li>
+                  ))}
+                </ul>
+              </nav>
+              <CrewInfo />
+            </div>
+          </div>
+        </section>
+      )} */}
+       <section className="crew">
+          <h2 className="crew__subtitle">
+            <span>02</span>Meet your crew
+          </h2>
+          <div className="crew__container">
+            <img
+              className="crew__image"
+              src="/images/crew/image-douglas-hurley.png"
+            />
+            <hr />
+            <div className="crew__info">
+              <nav className="crew__nav">
+                <ul className="crew__nav__lis">
+                  {crews.map((item, index) => (
+                    // <li
+                    //   key={index + 1}
+                    //   className={`${crewName === item && "active-crew"}`}
+                    //   onClick={() => handleCrew(item)}
+                    // ></li>
+                    <li></li>
+                  ))}
+                </ul>
+              </nav>
+              <CrewInfo />
+            </div>
+          </div>
+        </section>
+    </>
+  );
+};
+
+export default Crew;
